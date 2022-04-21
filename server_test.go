a25f5e4385b8b28c2a955b4a052a60e77211a8e7
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-22 06:11:48 +0700

    mengambil beberapa value dari request client

--- server_test.go
+++ server_test.go
@@ -6,15 +6,15 @@ import (
 	"testing"
 )
 
-func TestServerHTTP(t *testing.T) {
+func TestGetRequest(t *testing.T) {
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Home page")
-	})
-	mux.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "About page")
+		fmt.Fprintln(w, "Home page")
+		fmt.Fprintln(w, "METHOD: ", r.Method) // [GET,POST,PUT,PATCH,DELETE]
+		fmt.Fprintln(w, "URL: ", r.URL)       // url
+
 	})
 
 	http.ListenAndServe(":1234", mux)
