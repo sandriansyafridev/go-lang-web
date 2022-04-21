4e77f2e36602bd38050cf2029c8d255f066245ac
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-22 06:37:28 +0700

    mengambil query params dari URL

--- server_test.go
+++ server_test.go
@@ -9,20 +9,47 @@ import (
 	"testing"
 )
 
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "HTTP Testing")
+func testHandlerSingleQueryParams(w http.ResponseWriter, r *http.Request) {
+	queryParams_Name := r.URL.Query().Get("name")
+	fmt.Fprint(w, "Query Params Name: ", queryParams_Name)
 }
 
-var target = "http://localhost:1234/test-handler"
+func testHandlerMultipleQueryParams(w http.ResponseWriter, r *http.Request) {
+	queryParams_Name := r.URL.Query().Get("name")
+	queryParams_Age := r.URL.Query().Get("age")
+	fmt.Fprintln(w, "Query Params Name: ", queryParams_Name)
+	fmt.Fprintln(w, "Query Params Age: ", queryParams_Age)
+}
 
-func TestHTTPTest(t *testing.T) {
+var targetSingleQueryParams = "http://localhost:1234/test-handler?name=Sandrian"
+var targetMultipleQueryParams = "http://localhost:1234/test-handler?name=Sandrian&age=18"
+
+func TestQueryParams(t *testing.T) {
 
 	//create http test
-	request := httptest.NewRequest("GET", target, nil)
+	request := httptest.NewRequest("GET", targetSingleQueryParams, nil)
 	recorder := httptest.NewRecorder()
 
 	//run test
-	testHandler(recorder, request)
+	testHandlerSingleQueryParams(recorder, request)
+
+	//get response test
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	//log  response test to console
+	log.Println(string(body))
+
+}
+
+func TestMultipleQueryParams(t *testing.T) {
+
+	//create http test
+	request := httptest.NewRequest("GET", targetMultipleQueryParams, nil)
+	recorder := httptest.NewRecorder()
+
+	//run test
+	testHandlerMultipleQueryParams(recorder, request)
 
 	//get response test
 	response := recorder.Result()
