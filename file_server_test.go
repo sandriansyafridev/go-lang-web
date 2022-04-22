b6fb5d402a7fd1755306b1daa2ac133d044459e7
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-23 01:18:33 +0700

    template action with

--- file_server_test.go
+++ file_server_test.go
@@ -13,24 +13,26 @@ import (
 //go:embed templates/*.html
 var templates embed.FS
 
-func testHandler_TemplateAction_Range(w http.ResponseWriter, r *http.Request) {
+func testHandler_TemplateAction_With(w http.ResponseWriter, r *http.Request) {
 
 	t := template.Must(template.ParseFS(templates, "templates/*.html"))
 
 	t.ExecuteTemplate(w, "main.html", map[string]interface{}{
-		"Items": []string{
-			"Item 1", "Item 2", "Item 3", "Item 4",
+		"Name": "Sandrian",
+		"Address": map[string]interface{}{
+			"Street": "Jl.Guru",
+			"City":   "Jambi",
 		},
 	})
 
 }
 
-func TestTemplateAction_Range(t *testing.T) {
+func TestTemplateAction_With(t *testing.T) {
 
 	request := httptest.NewRequest(http.MethodGet, "http://localhost:1234/", nil)
 	recorder := httptest.NewRecorder()
 
-	testHandler_TemplateAction_Range(recorder, request)
+	testHandler_TemplateAction_With(recorder, request)
 
 	response := recorder.Result()
 	body, _ := io.ReadAll(response.Body)
