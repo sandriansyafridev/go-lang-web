00f31a571fc167b6a481cbd36e17b62cd973e095
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-23 00:37:58 +0700

    template with data

--- file_server_test.go
+++ file_server_test.go
@@ -10,22 +10,52 @@ import (
 	"text/template"
 )
 
-//go:embed templates/*.gohtml
+//go:embed templates/*.html
 var templates embed.FS
 
-func testHandler_SimpleHTML_Directory_Embed(w http.ResponseWriter, r *http.Request) {
+func testHandler_TemlateData_Map(w http.ResponseWriter, r *http.Request) {
 
-	t := template.Must(template.ParseFS(templates, "templates/*.gohtml"))
-	t.ExecuteTemplate(w, "main.gohtml", "Hello")
+	t := template.Must(template.ParseFS(templates, "templates/*.html"))
+
+	t.ExecuteTemplate(w, "main.html", map[string]interface{}{
+		"Title":  "Template",
+		"Header": "Hello World!",
+	})
 
 }
 
-func TestSimpleHTML_DirectoryWithEmbed(t *testing.T) {
+type Address struct {
+	City   string
+	Street string
+}
+
+type Page struct {
+	Title  string
+	Header string
+	Adress Address
+}
+
+func testHandler_TemlateData_Struct(w http.ResponseWriter, r *http.Request) {
+
+	t := template.Must(template.ParseFS(templates, "templates/*.html"))
+
+	t.ExecuteTemplate(w, "main.html", Page{
+		Title:  "Template Struct",
+		Header: "Hello World!",
+		Adress: Address{
+			Street: "Jl.Guru",
+			City:   "Jambi",
+		},
+	})
+
+}
+
+func TestTemplateData_Struct(t *testing.T) {
 
 	request := httptest.NewRequest(http.MethodGet, "http://localhost:1234/", nil)
 	recorder := httptest.NewRecorder()
 
-	testHandler_SimpleHTML_Directory_Embed(recorder, request)
+	testHandler_TemlateData_Struct(recorder, request)
 
 	response := recorder.Result()
 	body, _ := io.ReadAll(response.Body)
