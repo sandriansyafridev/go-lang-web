9cf07fb2544efe7480ebd921c4385f37eabb950a
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-23 04:10:35 +0700

    template struct method

--- file_server_test.go
+++ file_server_test.go
@@ -1,44 +1,51 @@
 package golangweb
 
 import (
+	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"text/template"
 )
 
-func testHandler_TemplateLayout(w http.ResponseWriter, r *http.Request) {
+type Person struct {
+	Name string
+}
+
+func (p Person) SayHelloTo(name string) string {
+	str := fmt.Sprintf("Hello %s, my name is %s", name, p.Name)
+	return str
+}
+
+func testHandler_TemplateFunction(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles(
-		"./templates/header.html",
 		"./templates/main.html",
+		"./templates/header.html",
 		"./templates/content.html",
 		"./templates/footer.html",
 	))
 
-	t.ExecuteTemplate(w, "main", map[string]interface{}{
-		"Title":  "Template layout",
-		"Header": "Hello World!",
-		"Items": []string{
-			"Item 1",
-			"Item 2",
-			"Item 3",
-			"Item 4",
+	data := map[string]interface{}{
+		"Title": "Function",
+		"Person": Person{
+			Name: "Sandrian",
 		},
-	})
+	}
+
+	t.ExecuteTemplate(w, "main", data)
 }
 
-func TestTemplateLayout(t *testing.T) {
+func TestTemplateFunction(t *testing.T) {
 
-	request := httptest.NewRequest(http.MethodGet, "http://localhost:1234/", nil)
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:1234/test", nil)
 	recorder := httptest.NewRecorder()
 
-	testHandler_TemplateLayout(recorder, request)
+	testHandler_TemplateFunction(recorder, request)
 
 	response := recorder.Result()
 	body, _ := io.ReadAll(response.Body)
 
 	log.Println(string(body))
-
 }
