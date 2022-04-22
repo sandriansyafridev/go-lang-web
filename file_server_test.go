51e7cf7b96ae4f4d89bfe0a1ffbf25291918a038
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-23 01:03:44 +0700

    template action if

--- file_server_test.go
+++ file_server_test.go
@@ -13,38 +13,22 @@ import (
 //go:embed templates/*.html
 var templates embed.FS
 
-type Address struct {
-	City   string
-	Street string
-}
-
-type Page struct {
-	Title  string
-	Header string
-	Adress Address
-}
-
-func testHandler_TemlateData_IfElse(w http.ResponseWriter, r *http.Request) {
+func testHandler_TemplateAction(w http.ResponseWriter, r *http.Request) {
 
 	t := template.Must(template.ParseFS(templates, "templates/*.html"))
 
-	t.ExecuteTemplate(w, "main.html", Page{
-		Title:  "Template IfElse",
-		Header: "Hello World!",
-		Adress: Address{
-			Street: "Jl.Guru",
-			City:   "Jambi",
-		},
+	t.ExecuteTemplate(w, "main.html", map[string]interface{}{
+		"Value": 75,
 	})
 
 }
 
-func TestTemplateData_Struct(t *testing.T) {
+func TestTemplateAction(t *testing.T) {
 
 	request := httptest.NewRequest(http.MethodGet, "http://localhost:1234/", nil)
 	recorder := httptest.NewRecorder()
 
-	testHandler_TemlateData_IfElse(recorder, request)
+	testHandler_TemplateAction(recorder, request)
 
 	response := recorder.Result()
 	body, _ := io.ReadAll(response.Body)
