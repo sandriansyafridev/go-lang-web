aca01cdbadb4d93c7e996d28bc6cc824cd53d38d
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-23 01:12:51 +0700

    template action with range

--- file_server_test.go
+++ file_server_test.go
@@ -13,22 +13,24 @@ import (
 //go:embed templates/*.html
 var templates embed.FS
 
-func testHandler_TemplateAction(w http.ResponseWriter, r *http.Request) {
+func testHandler_TemplateAction_Range(w http.ResponseWriter, r *http.Request) {
 
 	t := template.Must(template.ParseFS(templates, "templates/*.html"))
 
 	t.ExecuteTemplate(w, "main.html", map[string]interface{}{
-		"Value": 75,
+		"Items": []string{
+			"Item 1", "Item 2", "Item 3", "Item 4",
+		},
 	})
 
 }
 
-func TestTemplateAction(t *testing.T) {
+func TestTemplateAction_Range(t *testing.T) {
 
 	request := httptest.NewRequest(http.MethodGet, "http://localhost:1234/", nil)
 	recorder := httptest.NewRecorder()
 
-	testHandler_TemplateAction(recorder, request)
+	testHandler_TemplateAction_Range(recorder, request)
 
 	response := recorder.Result()
 	body, _ := io.ReadAll(response.Body)
